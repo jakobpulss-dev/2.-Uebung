ce163b2c5b171262c540d0df8c9ba9a3d6ed3485
jakobpulss-dev <jakobpulss@gmail.com>
2025-10-31 08:33:54 +0100

    Testwert

---
+++ src/p2/p2_main.java
@@ -0,0 +1,17 @@
+package p2;
+
+public class p2_main { public static void main(String[] args) {
+    int i = 5;          // i bekommt einen beliebigen Wert
+    boolean b = false;   // b bekommt einen beliebigen Anfangswert
+
+    // Überprüfung, ob i echt größer als 10 ist
+    if (i > 10) {
+        b = true;        // b wird nur geändert, wenn Bedingung erfüllt ist
+    }
+
+    System.out.println("i = " + i);
+    System.out.println("b = " + b);
+}
+};
+
+
